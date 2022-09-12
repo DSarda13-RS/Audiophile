9b15cbaf278d83507f7594d2541d6fef23ec48b4
DevSarda <dev.sarda@remotestate.com>
2022-09-12 14:52:11 +0530

    third commit

--- src/tests/allUser.test.ts
+++ src/tests/allUser.test.ts
@@ -15,23 +15,23 @@ beforeAll(async () => {
 });
 
 describe("POST /user/register", () => {
-    it("should return 400",  async() => {
+    it("should return 400, if uploaded file is not image",  async() => {
         const response = await request(baseURL).post("/user/register").attach('file',path.resolve(__dirname,'DS.txt')).field({'name':'Dev','email':'devsarda789@gmail.com','password':'test1234','address':'Gotham City1'})
         await expect(response.statusCode).toBe(400);
     });
-    it("should return 400",  async() => {
+    it("should return 400, if provided name is invalid",  async() => {
         const response = await request(baseURL).post("/user/register").attach('file',path.resolve(__dirname,'DS.jpg')).field({'name':123,'email':'devsarda789@gmail.com','password':'test1234','address':'Gotham City1'})
         await expect(response.statusCode).toBe(400);
     });
-    it("should return 400",  async() => {
+    it("should return 400, if provided email is invalid",  async() => {
         const response = await request(baseURL).post("/user/register").attach('file',path.resolve(__dirname,'DS.jpg')).field({'name':'Dev','email':'devsarda789gmail.com','password':'test1234','address':'Gotham City1'})
         await expect(response.statusCode).toBe(400);
     });
-    it("should return 400",  async() => {
+    it("should return 400, if provided password is invalid",  async() => {
         const response = await request(baseURL).post("/user/register").attach('file',path.resolve(__dirname,'DS.jpg')).field({'name':'Dev','email':'devsarda789@gmail.com','password':'tes1234','address':'Gotham City1'})
         await expect(response.statusCode).toBe(400);
     });
-    it("should return 400",  async() => {
+    it("should return 400, if address is not provided",  async() => {
         const response = await request(baseURL).post("/user/register").attach('file',path.resolve(__dirname,'DS.jpg')).field({'name':'Dev','email':'devsarda789@gmail.com','password':'test1234'})
         await expect(response.statusCode).toBe(400);
     });
@@ -39,7 +39,7 @@ describe("POST /user/register", () => {
         const response = await request(baseURL).post("/user/register").attach('file',path.resolve(__dirname,'DS.jpg')).field({'name':'Dev','email':'devsarda789@gmail.com','password':'test1234','address':'Gotham City1'})
         await expect(response.statusCode).toBe(200);
     });
-    it("should return 409",  async() => {
+    it("should return 409, if email already used",  async() => {
         const response = await request(baseURL).post("/user/register").attach('file',path.resolve(__dirname,'DS.jpg')).field({'name':'Dev','email':'devsarda789@gmail.com','password':'test1234','address':'Gotham City1'})
         await expect(response.statusCode).toBe(409);
     });
